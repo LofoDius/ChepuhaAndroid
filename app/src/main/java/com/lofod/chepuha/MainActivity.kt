f7db730bc554b6cd6f575ccff129d9c970429f19
LofoD <evkaverin@gmail.com>
2021-06-22 12:52:33 +0300

    Initial commit

---
+++ app/src/main/java/com/lofod/chepuha/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lofod.chepuha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
